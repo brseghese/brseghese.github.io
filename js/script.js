d661c0050fc9170c03698ea276cff5ff0dc97d01
brseghese <brseghese@gmail.com>
2022-03-24 21:12:58 -0300

    icones e memory game

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// const typing = document.querySelector("#nome");
+
+// let message = "Bruno Seghese";
+
+// let characterIndex = 0;
+// let currentCharacters = "";
+
+// const type = () => {
+//   currentCharacters = message.slice(0, characterIndex++);
+//   typing.textContent = currentCharacters;
+//   if (characterIndex === 14) {
+//     characterIndex = 0;
+//   }
+// };
+
+// setInterval(type, 200);
